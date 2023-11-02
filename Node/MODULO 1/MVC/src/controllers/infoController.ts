a666daba8ad38f9c2e07b99934838dabd812e33d
Hugo <hugo.html.com@gmail.com>
2023-11-02 16:12:36 -0300

    feat: HTMl E CSS

--- Node/MODULO 1/MVC/src/controllers/infoController.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const contato = (req:Request, res:Response)=>{
-    res.render('contato');
-}
-
-export const sobre = (req:Request, res:Response)=>{
-    res.render('sobre');
-}
